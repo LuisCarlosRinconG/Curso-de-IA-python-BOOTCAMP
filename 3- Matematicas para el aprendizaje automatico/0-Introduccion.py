dd4031e7d0e599eee401c4324117b1321c1a2eeb
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2025-09-11 20:24:45 -0500

    Create 0-Introduccion.py

---
+++ 3- Matematicas para el aprendizaje automatico/0-Introduccion.py
@@ -0,0 +1,9 @@
+'''
+1-Fundamentos del algebra lineal
+2-Conceptos avanzados del algebra lineal
+3-Calculo para el aprendizaje automatico-derivadas
+4-Calculos para el aprendizaje automatico - Integrales y optimización
+5-Teoria de la probabilidad y distribuciones
+6-Fundamentos de estadistica
+7-Proyecto matematico impulsado por matematicas sobre regresión lineal
+'''
